836f092a6dd37f881c7838e7aa93a5cd21b54431
Mitsutoshi Aoe <maoe@foldr.in>
2014-08-26 23:37:01 +0900

    benchmarks: Upgrade criterion to use env

--- benchmarks/ping-pong-latency.hs
+++ benchmarks/ping-pong-latency.hs
@@ -1,29 +1,38 @@
 {-# LANGUAGE BangPatterns #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+import Control.Applicative
 import Control.Concurrent.Chan
 import Control.Concurrent.STM
 import Control.Monad
+
+import Control.DeepSeq (NFData(..))
 
 import Criterion.Main
 import Control.Concurrent.Async
 
 iterations :: Int
-iterations = 1000
+iterations = 100
 
 main :: IO ()
 main = defaultMain
-  [ bench "Chan" $ whnfIO benchmarkChan
-  , bench "TChan" $ whnfIO benchmarkTChan
-  , bench "TQueue" $ whnfIO benchmarkTQueue
-  , bench "TBQueue" $ whnfIO benchmarkTBQueue
+  [ env setupChan $ \ ~(pingQ, pongQ) ->
+      bench "Chan" $ whnfIO $ benchmarkChan pingQ pongQ
+  , env setupTChan $ \ ~(pingQ, pongQ) ->
+      bench "TChan" $ whnfIO $ benchmarkTChan pingQ pongQ
+  , env setupTQueue $ \ ~(pingQ, pongQ) ->
+      bench "TQueue" $ whnfIO $ benchmarkTQueue pingQ pongQ
+  , env setupTBQueue $ \ ~(pingQ, pongQ) ->
+      bench "TBQueue" $ whnfIO $ benchmarkTBQueue pingQ pongQ
   ]
 
 -------------------------------------------------
 -- Chan
 
-benchmarkChan :: IO ()
-benchmarkChan = do
-  pingQ <- newChan
-  pongQ <- newChan
+setupChan :: IO (Chan Int, Chan Int)
+setupChan = (,) <$> newChan <*> newChan
+
+benchmarkChan :: Chan Int -> Chan Int -> IO ()
+benchmarkChan pingQ pongQ =
   withAsync (pongerChan pingQ pongQ) $ \_ ->
     pingerChan pingQ pongQ iterations
 
@@ -49,10 +58,11 @@ pongerChan pingQ pongQ = forever $
 -------------------------------------------------
 -- TChan
 
-benchmarkTChan :: IO ()
-benchmarkTChan = do
-  pingQ <- newTChanIO
-  pongQ <- newTChanIO
+setupTChan :: IO (TChan Int, TChan Int)
+setupTChan = atomically $ (,) <$> newTChan <*> newTChan
+
+benchmarkTChan :: TChan Int -> TChan Int -> IO ()
+benchmarkTChan pingQ pongQ =
   withAsync (pongerTChan pingQ pongQ) $ \_ ->
     pingerTChan pingQ pongQ iterations
 
@@ -78,10 +88,11 @@ pongerTChan pingQ pongQ = forever $
 -------------------------------------------------
 -- TQueue
 
-benchmarkTQueue :: IO ()
-benchmarkTQueue = do
-  pingQ <- newTQueueIO
-  pongQ <- newTQueueIO
+setupTQueue :: IO (TQueue Int, TQueue Int)
+setupTQueue = atomically $ (,) <$> newTQueue <*> newTQueue
+
+benchmarkTQueue :: TQueue Int -> TQueue Int -> IO ()
+benchmarkTQueue pingQ pongQ =
   withAsync (pongerTQueue pingQ pongQ) $ \_ ->
     pingerTQueue pingQ pongQ iterations
 
@@ -107,10 +118,11 @@ pongerTQueue pingQ pongQ = forever $
 -------------------------------------------------
 -- TBQueue
 
-benchmarkTBQueue :: IO ()
-benchmarkTBQueue = do
-  pingQ <- newTBQueueIO 1024
-  pongQ <- newTBQueueIO 1024
+setupTBQueue :: IO (TBQueue Int, TBQueue Int)
+setupTBQueue = atomically $ (,) <$> newTBQueue 1024 <*> newTBQueue 1024
+
+benchmarkTBQueue :: TBQueue Int -> TBQueue Int -> IO ()
+benchmarkTBQueue pingQ pongQ =
   withAsync (pongerTBQueue pingQ pongQ) $ \_ ->
     pingerTBQueue pingQ pongQ iterations
 
@@ -132,3 +144,10 @@ pongerTBQueue
   -> IO ()
 pongerTBQueue pingQ pongQ = forever $
   atomically (readTBQueue pingQ) >>= atomically . writeTBQueue pongQ
+
+-------------------------------------------------
+
+instance NFData (Chan a)
+instance NFData (TChan a)
+instance NFData (TQueue a)
+instance NFData (TBQueue a)
